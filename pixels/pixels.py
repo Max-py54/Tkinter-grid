ce29aae246e4e5844130453ff570ef8a3e9bfcd5
Max.py <87494082+Max-py54@users.noreply.github.com>
2022-04-08 22:09:02 +0200

    Update pixels.py

--- pixels/pixels.py
+++ pixels/pixels.py
@@ -1,9 +1,9 @@
-"""A module to help creating and managing a grid wit pixels."""
+"""A module to help creating and managing a grid with pixels."""
 
 import tkinter as tk
 
 class Pixel():
-    def __init__(self, canvas, line:int, column:int, width_line:int, width_column:int, colour:str="black"):
+    def __init__(self, canvas, line:int, column:int, width_line:int, width_column:int, colour:str="black", outline:str="black"):
         self.canvas = canvas
         
         self.line = line
@@ -13,17 +13,18 @@ class Pixel():
         self.width_column = width_column
 
         self.colour = colour
+        self.outline = outline
 
         self.create()
 
     def create(self):
-        x1 = self.column*self.width_column
-        y1 = self.line*self.width_line
+        y1 = self.column*self.width_column
+        x1 = self.line*self.width_line
         
-        x2 = self.column*self.width_column+self.width_column
-        y2 = self.line*self.width_line+self.width_line
+        y2 = self.column*self.width_column+self.width_column
+        x2 = self.line*self.width_line+self.width_line
         
-        self.pixel = self.canvas.create_rectangle(x1, y1, x2, y2, fill=self.colour, outline=self.colour)
+        self.pixel = self.canvas.create_rectangle(x1, y1, x2, y2, fill=self.colour, outline=self.outline)
 
     def delete(self):
         self.canvas.delete(self.pixel)
@@ -48,19 +49,29 @@ Creates a grid."""
 
         self.pixels = []
 
-    def pixel(self, line:int, column:int, colour:str="black"):
-        """Grid.pixel(line:int, column:int, colour:str="black")
+    def pixel(self, line:int, column:int, colour:str="black", outline:str=""):
+        """Grid.pixel(line:int, column:int, colour:str="black", outline:str="")
 Changes a specific pixel's colour."""
+        if outline == "":
+            outline = colour
         if self.pixels != []:
             for i, px in enumerate(self.pixels):
                 if px.line == line and px.column == column:
                     px.delete()
                     self.pixels.pop(i)
                     break
-            self.pixels.append(Pixel(self.canvas, line, column, self.width_line, self.width_column, colour))
+            if colour != self.colour:
+                self.pixels.append(Pixel(self.canvas, line, column, self.width_line, self.width_column, colour, outline))
         else:
             if colour != self.colour:
-                self.pixels.append(Pixel(self.canvas, line, column, self.width_line, self.width_column, colour))
+                self.pixels.append(Pixel(self.canvas, line, column, self.width_line, self.width_column, colour, outline))
+
+    def clear(self):
+        """Clears the entire grid."""
+        if self.pixels != []:
+            for px in self.pixels:
+                px.delete()
+            self.pixels = []
             
 
 if __name__ == "__main__":
@@ -75,6 +86,9 @@ if __name__ == "__main__":
 
     my_grid = Grid(window, lines, columns, width, height)
 
-    my_grid.pixel(0, 1, "red")
+    my_grid.pixel(7, 7, "red")
+    my_grid.pixel(7, 5, "blue")
+
+    my_grid.canvas.after(1000, my_grid.clear)
     
     window.mainloop()
